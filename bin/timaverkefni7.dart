2688a3d5f0175381f77b26ff424def404b1caaaa
Tómas <tomashg1985@gmail.com>
2021-10-15 20:46:41 +0000

    first project draft

---
+++ bin/timaverkefni7.dart
@@ -0,0 +1,16 @@
+
+  void main(List<String> arguments) {
+
+  // The best way to solve a complex problem is to break it into smaller problems.
+  // Functions allow us to break a complicated algorithm into smaller ones.
+  // You have already been using functions so far, the void main(List<String> arguments) {} is a function.
+  // That is your main function, the program itself. But you can create other functions outside of the main one.
+  // And call for them to use inside your main one. Keeps your main function cleaner and simpler.
+
+  // Built in functions you are already familiar with. everytime you use .something you are using a built in function.
+  // item.length and item.toUpperCase are samples of them.
+  // The ones you create yourself are called User-defined functions.
+
+
+
+}
